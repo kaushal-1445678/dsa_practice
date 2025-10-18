dbb7bb854596527879770abd9e142f9a82da8d54
kaushal-1445678 <kaushal730223@gmail.com>
2025-10-18 20:16:37 +0000

    solve two sum problem

---
+++ twoSum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class main{
+public static void main(String[] args) {
+    int[] a={2,4,5,6,7};
+    int target=8;
+    boolean ans=twoSum(a,target);
+    System.out.println(ans);
+    
+}
+public static boolean twoSum(int[] a,int target){
+    Arrays.sort(a);
+    int n=a.length;
+    int left=0;
+    int right=n-1;
+    while(left<right){
+        int sum=a[left]+a[right];
+        
+            if(sum==target){
+                return true;
+            }else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        
+    }
+    return false;
+
+}
+}
