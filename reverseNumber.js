69d70dcc709d7197802e51017646bf41e3f341bf
kaushal-1445678 <kaushal730223@gmail.com>
2025-12-26 18:10:36 +0000

    solve reverse Number in javascript

---
+++ reverseNumber.js
@@ -0,0 +1,17 @@
+
+let a=12;
+let ans=reverseNumber(a);
+
+console.log(ans);
+function reverseNumber(a){
+    let temp=a;
+    let rv=0;
+    while(a!==0){
+        let dig=a%10;
+        rv=rv*10+dig;
+        a=Math.floor(a/10);
+        
+    }
+    return rv;
+
+}
