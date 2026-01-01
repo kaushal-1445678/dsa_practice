ffb5832a1fdfdca4b01ac57720c99587315d88a2
kaushal-1445678 <kaushal730223@gmail.com>
2026-01-01 16:22:07 +0000

    code reversenumber in js in arrow function

--- reverseNumber.js
+++ reverseNumber.js
@@ -1,17 +1,29 @@
 
-let a=12;
-let ans=reverseNumber(a);
+// let a=12;
+// let ans=reverseNumber(a);
 
-console.log(ans);
-function reverseNumber(a){
-    let temp=a;
-    let rv=0;
-    while(a!==0){
-        let dig=a%10;
-        rv=rv*10+dig;
-        a=Math.floor(a/10);
+// console.log(ans);
+// function reverseNumber(a){
+//     let temp=a;
+//     let rv=0;
+//     while(a!==0){
+//         let dig=a%10;
+//         rv=rv*10+dig;
+//         a=Math.floor(a/10);
         
-    }
-    return rv;
+//     }
+//     return rv;
+
+// }
+const reverseNumber=(n)=>{
+    let rv=0;
+     while(n!==0){
+        let dig=n%10;
+        rv=rv*10+dig;
+        n=Math.floor(n/10);
+
+     }
+     return rv;
 
 }
+console.log(reverseNumber(1234));
