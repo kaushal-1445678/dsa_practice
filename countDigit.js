78bdc967dd2a84aa3d742279332f6df93d15ef92
kaushal-1445678 <kaushal730223@gmail.com>
2026-01-01 16:11:18 +0000

    code countdigit in js arrow function

--- countDigit.js
+++ countDigit.js
@@ -1,12 +1,26 @@
-function countDigit(a){
+// function countDigit(a){
+//     let cnt=0;
+//     while(a!==0){
+//         cnt++;
+//         a=Math.floor(a/10);
+//     }
+//     return cnt;
+
+// }
+// let a=12121212
+// let ans=countDigit(a);
+// console.log(ans)
+
+const countDigit =(n)=> {
     let cnt=0;
-    while(a!==0){
+    while(n>0){
         cnt++;
-        a=Math.floor(a/10);
+        n=Math.floor(n/10);
     }
     return cnt;
 
 }
-let a=12121212
-let ans=countDigit(a);
-console.log(ans)
+let n=1212898945;
+
+console.log(countDigit(n));
+
