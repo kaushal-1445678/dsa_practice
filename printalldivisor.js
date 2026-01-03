458948600d2c3a8e4da13f003cd24597a175e9f5
kaushal-1445678 <kaushal730223@gmail.com>
2026-01-03 18:52:40 +0000

    code printAlldivisor in js

---
+++ printalldivisor.js
@@ -0,0 +1,16 @@
+let n=36;
+
+
+const getAllDivisor=(n)=>{
+    let divisor=[];
+    for(let i=1;i*i<=n;i++){
+        if(n%i===0){
+            divisor.push(i);
+            if(i!==n/i){
+                divisor.push(n/i);
+            }
+        }
+    }
+    return divisor;
+}
+console.log(getAllDivisor(n));
