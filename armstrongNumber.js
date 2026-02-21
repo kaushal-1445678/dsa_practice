4b168341ec5c014afef5da3c749a7c7e4a2532dc
kaushal-1445678 <kaushal730223@gmail.com>
2026-02-21 19:52:07 +0000

    one more way to write armstrong number

--- armstrongNumber.js
+++ armstrongNumber.js
@@ -1,17 +1,43 @@
-const isArm = (n)=>{
-    let temp=n;
-    let sum=0;
-    let k=String(n).length;
-    while(temp!==0){
-        let dig=temp%10;
-        sum+=Math.pow(dig,k);
-        temp=Math.trunc(temp/10);
-    }
-    if(n===sum){
-        return true;
-    }
-    return false;
+// const isArm = (n)=>{
+//     let temp=n;
+//     let sum=0;
+//     let k=String(n).length;
+//     while(temp!==0){
+//         let dig=temp%10;
+//         sum+=Math.pow(dig,k);
+//         temp=Math.trunc(temp/10);
+//     }
+//     if(n===sum){
+//         return true;
+//     }
+//     return false;
 
-}
-let n=153;
-console.log(isArm(n));
+// }
+// let n=153;
+// console.log(isArm(n));
+
+
+// const isA=(n)=>{
+//     let cnt=0;
+//     let temp=n;
+//     while(temp!=0){
+//         cnt++;
+//         temp=Math.trunc(temp/10);
+//     }
+//     let m=n;
+//     let ans=0;
+//     while(m!=0){
+//         let dig=m%10;
+//         ans=ans+Math.pow(dig,cnt);
+//         m=Math.trunc(m/10);
+
+
+//     }
+//     if(ans==n){
+//         return true;
+//     }
+//     return false;
+// }
+
+// let n=153;
+// console.log(isA(n));
