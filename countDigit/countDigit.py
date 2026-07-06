2eb3b4f46d4163559a39d3581dd6bfaa4d9aa150
kaushal-1445678 <kaushal730223@gmail.com>
2026-07-06 23:25:45 +0000

    .

--- countDigit/countDigit.py
+++ countDigit/countDigit.py
@@ -1,9 +1,9 @@
-print("hello world");
+
 def count_digit(n):
     cnt=0
     while n!=0:
         cnt+=1
         n=n//10;
     return cnt
-print(count_digit(7878090896))
+print(count_digit(78780908965))
 
