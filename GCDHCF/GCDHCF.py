bd42f27c49f90abbb6ad56969509aa21d195fdb9
kaushal-1445678 <kaushal730223@gmail.com>
2026-07-07 01:55:03 +0000

    code GCDHCF in python.

---
+++ GCDHCF/GCDHCF.py
@@ -0,0 +1,11 @@
+def GCDHCF(n1,n2):
+    while(n1>0 and n2>0):
+        if n1>n2:
+            n1=n1%n2
+        else:
+            n2=n2%n1
+
+    if(n1==0):
+        return n2
+    return n1
+print(GCDHCF(9,12))
