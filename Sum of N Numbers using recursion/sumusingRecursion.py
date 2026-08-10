b8fde563a26c9ff2c74d8cd8672a1945665e9839
kaushal-1445678 <kaushal730223@gmail.com>
2026-08-10 22:23:16 +0000

    code sum of  n numbers in a functional way

--- Sum of N Numbers using recursion/sumusingRecursion.py
+++ Sum of N Numbers using recursion/sumusingRecursion.py
@@ -1,8 +1,19 @@
-def sumOfNNumbers(sum,i):
-    if i==0:
-        return sum
-    return sumOfNNumbers(sum+i,i-1)
+# parameterised way
+# def sumOfNNumbers(sum,i):
+#     if i==0:
+#         return sum
+#     return sumOfNNumbers(sum+i,i-1)
+
+# n=5
+# sum=0
+# print(sumOfNNumbers(sum,n))
+
+# functional way
+def sumOfNNumbers(n):
+    if(n==0):
+        return 0
+    return n+sumOfNNumbers(n-1)
 
 n=5
-sum=0
-print(sumOfNNumbers(sum,n))
+
+print(sumOfNNumbers(n))
